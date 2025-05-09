02040435575d8c61682e186c09875b27852463c6
minecoby <127065808+minecoby@users.noreply.github.com>
2025-05-10 00:14:39 +0900

    [Silver II] Title: N과 M (9), Time: 124 ms, Memory: 37024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,23 @@
+N, M = map(int,input().split())
+board = list(map(int,input().split()))
+arr = []
+used = [False] * N
+used_set = set()
+def func(k):
+    global used_set
+    t = tuple(arr)
+    if k == M and t not in used_set:
+        used_set.add(t)
+        print(*t)
+        return
+    for i in range(len(board)):
+        if used[i] == False:
+            arr.append(board[i])
+            used[i] = True
+            func(k+1)
+            arr.pop()
+            used[i] = False
+
+board.sort()
+func(0)
+

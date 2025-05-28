52346a8a1ff64f0100920b04bdec88f3d182ea2a
minecoby <127065808+minecoby@users.noreply.github.com>
2025-05-28 11:28:58 +0900

    [Silver II] Title: 최소 힙, Time: 164 ms, Memory: 114216 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,13 @@
+import heapq
+import sys
+N = int(sys.stdin.readline().rstrip())
+heap = []
+for _ in range(N):
+    cmd = int(sys.stdin.readline().rstrip())
+    if cmd == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap,cmd)

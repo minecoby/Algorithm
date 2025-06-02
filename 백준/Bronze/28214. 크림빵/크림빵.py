7e6eb95fec3d5d98b1edc5165e492d2b63437daa
minecoby <127065808+minecoby@users.noreply.github.com>
2025-06-03 01:11:01 +0900

    [Bronze III] Title: 크림빵, Time: 48 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -1,10 +1,10 @@
-N,K,P = map(int,input().split())
-
-arr = list(map(int,input().split()))
+N, K, P = map(int, input().split())
+arr = list(map(int, input().split()))
 
 ans = 0
-for i in range(0,N,K):
-    a = arr[i:K].count(0)
-    if a < P:
+for i in range(0, N * K, K):
+    if len(arr[i:i+K]) < K:
+        continue  
+    if arr[i:i+K].count(0) < P:
         ans += 1
 print(ans)

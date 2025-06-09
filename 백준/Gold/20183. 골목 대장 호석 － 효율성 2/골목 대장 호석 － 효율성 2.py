9be26d5cafa9332a15751fe31bf2d27246158f7b
minecoby <127065808+minecoby@users.noreply.github.com>
2025-06-09 13:50:47 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 16692 ms, Memory: 221960 KB, Score: 19 point -BaekjoonHub

---
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
@@ -0,0 +1,33 @@
+from heapq import heappush, heappop
+
+N,M,A,B,C = map(int,input().split())
+
+def dijkstra():
+    heap_list = []
+    heappush(heap_list,(0,0,A)) #거리값, 수치심, 경로? 
+    distance[A][1] = 0
+    
+    while heap_list:
+        weight,max_weight,vertex = heappop(heap_list)
+        if distance[vertex][1] != weight: continue
+        for info in path[vertex]:
+            new_weight = weight + info[0]
+            max_weight = max(max_weight,info[0])
+            if new_weight > C: continue
+            if distance[info[1]][0] <= max_weight:continue
+            distance[info[1]][0] = max_weight
+            distance[info[1]][1] = new_weight
+            heappush(heap_list,(new_weight,max_weight,info[1]))
+    return distance[B][0] if distance[B][1] != float("inf") else -1
+                
+path = {}
+for i in range(1,N+1):
+    path[i] = []
+distance = [[float("inf"),float("inf")] for _ in range(N+1)]
+for _ in range(M):
+    u,v,w = map(int,input().split())
+    path[u].append([w,v])
+    path[v].append([w,u])
+
+ans = dijkstra()
+print(ans)

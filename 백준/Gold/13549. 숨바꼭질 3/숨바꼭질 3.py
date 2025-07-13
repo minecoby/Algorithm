5116835b238e328a8cad7e63ceeb53facd2d1b47
minecoby <127065808+minecoby@users.noreply.github.com>
2025-07-13 17:13:29 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 140 ms, Memory: 36604 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+MAX = 100000
+
+visited = [False] * (MAX+1)
+
+que = deque()
+que.append((N, 0))
+visited[N] = True
+
+while que:
+    x, t = que.popleft()
+    if x == K:
+        print(t)
+        sys.exit()        
+    nx = 2*x
+    if 0 <= nx <= MAX and not visited[nx]:
+        visited[nx] = True
+        que.append((nx, t))
+    for nx in (x-1, x+1):
+        if 0 <= nx <= MAX and not visited[nx]:
+            visited[nx] = True
+            que.append((nx, t+1))
+
+print(-1)

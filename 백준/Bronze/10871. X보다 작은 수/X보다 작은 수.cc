8fbcda698613b8d75e927bfc36cc6d32944eae99
minecoby <127065808+minecoby@users.noreply.github.com>
2025-09-24 17:27:49 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N,X;
+    cin >> N >> X;
+    vector<int> A(N);
+    for(int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    for(int i = 0; i < N; i++){
+        if(A[i] < X) cout << A[i] << " ";
+    }
+}

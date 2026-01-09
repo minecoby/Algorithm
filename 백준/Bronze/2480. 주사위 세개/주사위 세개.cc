b535ea8230a6ffafb591cd912d031f8d8691bbb2
minecoby <127065808+minecoby@users.noreply.github.com>
2026-01-09 14:35:43 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int a,b,c;
+	cin >> a >> b >> c;
+	if(a==b && b==c) cout << 10000+a*1000;
+	else if (a==b && b != c) cout << 1000 + a*100;
+	else if (a==c && a != b) cout << 1000 + a*100;
+	else if (b==c && a != b) cout << 1000 + b*100;
+	else{
+		int max_value = max(max(a,b),c);
+		cout << max_value * 100;
+	}
+
+	return 0;
+}

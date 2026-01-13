8a7faebe613b2e30da32031d689925bb947cad93
minecoby <127065808+minecoby@users.noreply.github.com>
2026-01-14 03:44:53 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int N;
+	cin >> N;
+
+	for(int i = 1; i <= N; i++){
+		for(int j = N-i; j > 0; j--) cout << " ";
+		for(int j = 0; j < i; j++) cout << "*";
+		cout << "\n";
+	}
+	return 0;
+}

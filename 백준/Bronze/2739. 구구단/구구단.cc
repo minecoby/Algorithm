e8682c9e624ae12cf0607fc6a874aa68e8fddd25
minecoby <127065808+minecoby@users.noreply.github.com>
2026-01-14 03:23:57 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int N;
+	cin >> N;
+
+	for(int i = 1; i <= 9; i++){
+		cout << N << " * " << i << " = " << N*i << "\n";
+	}
+	return 0;
+}

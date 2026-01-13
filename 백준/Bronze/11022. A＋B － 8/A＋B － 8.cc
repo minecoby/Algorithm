27a7cacbbf87f5b4e04f23808885cf59c027eac7
minecoby <127065808+minecoby@users.noreply.github.com>
2026-01-14 03:39:11 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int T, A, B;
+	cin >> T;
+	for(int i =1; i <= T; i++){
+		cin >> A >> B;
+		cout << "Case #" << i << ": " << A << " + " << B << " = " << A+B << "\n";
+	}
+	return 0;
+}

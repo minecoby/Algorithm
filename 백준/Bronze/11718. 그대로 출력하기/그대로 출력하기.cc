cc91cc685d75e9ec8f449a112d6a26926d5d67c2
minecoby <127065808+minecoby@users.noreply.github.com>
2026-01-26 21:53:33 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	string S;
+	while(getline(cin, S)){
+		cout << S << "\n";
+	}
+	return 0;
+}
